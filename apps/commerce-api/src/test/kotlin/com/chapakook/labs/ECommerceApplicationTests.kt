d9552201a7bc4c4e037f26d94e5df7e4f19ef616
chapakook <chapakook96@gmail.com>
2025-09-03 15:37:42 +0900

    feat(init): first commit

---
+++ apps/commerce-api/src/test/kotlin/com/chapakook/labs/ECommerceApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.chapakook.labs
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ECommerceApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
